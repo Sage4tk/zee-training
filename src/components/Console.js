ed0168daafbf3a4e8c251717e32c512075f80733
timothy timbol <ttimbol08@gmail.com>
2022-06-07 18:44:09 +0400

    added logos

--- src/components/Console.js
+++ src/components/Console.js
@@ -1,4 +1,4 @@
-import { ShieldFill } from "react-bootstrap-icons"
+import { ShieldFill, PersonFill, YinYang } from "react-bootstrap-icons"
 
 export default function Console() {
     return (
@@ -13,13 +13,13 @@ const NavBar = () => {
     return (
         <nav className="side-nav fixed-left bg-dark d-flex flex-column p-3">
             <div className="d-flex flex-row align-items-center">
-                <ShieldFill color="white" height="100%" width={32} className="mr-3" />
+                <ShieldFill color="white" height="100%" width={32} style={{marginRight: "1rem"}} />
                 <h2 className="text-white ml-3">PANEL</h2>
             </div>
             <hr  className="bg-light" style={{width: "100%", height:"1px"}} />
             <div className="navbar-nav">
-                <a className="nav-item nav-link active">Clients</a>
-                <a className="nav-item nav-link">Workouts</a>
+                <a className="nav-item nav-link active"><PersonFill style={{marginRight: "0.5em"}} />Clients</a>
+                <a className="nav-item nav-link"><YinYang style={{marginRight: "0.5em"}}/>Workouts</a>
             </div>
         </nav>
     )
